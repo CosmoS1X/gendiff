409eeac674d23eb16b61eef22d343578754171a3
Konstantin Korovin <cosmo.stan@yandex.com>
2024-12-25 16:27:53 +0300

    refactor: correct the reviewer's comments

--- src/formatters/index.ts
+++ src/formatters/index.ts
@@ -6,9 +6,11 @@ import type { Diff, FormattersUnion } from '../types';
 const formatters = { stylish, plain, json };
 
 export default (diff: Diff[], formatter: FormattersUnion): string => {
-  if (!Object.hasOwn(formatters, formatter)) {
+  const pickedFormatter = formatters[formatter];
+
+  if (!pickedFormatter) {
     throw new Error(`Unknown formatter name: '${formatter}'`);
   }
 
-  return formatters[formatter](diff);
+  return pickedFormatter(diff);
 };
